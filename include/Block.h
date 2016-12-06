840d0a9768ffa61d884a968172d3d9001db9e314
杨森 <yangsen758@foxmail.com>
2016-12-06 23:22:37 +0800

    improve the declaration of Basic Block.

--- include/Block.h
+++ include/Block.h
@@ -1,8 +1,9 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include <QuaterInstr.h>
+#include "QuaterInstr.h"
 #include <vector>
+#include <set>
 
 class Block
 {
@@ -10,9 +11,23 @@ class Block
         Block();
         virtual ~Block();
 
-		std::vector<QuaterInstr *> middleCode;
-		std::vector<Block *> before;
-		std::vector<Block *> after;
+		std::vector<QuaterInstr *> middleCode;	// 基本块中包含的四元式
+		std::vector<Block *> before;	// 前驱基本块数组
+		std::vector<Block *> after;		// 后继基本块数组
+
+		// Reaching Definetion 
+		std::vector<QuaterInstr *> rdOut;
+		std::vector<QuaterInstr *> rdIn;
+		std::vector<QuaterInstr *> gen;
+		std::vector<QuaterInstr *> kill;
+
+		// live-variable analysis	in[B] = use[B] U (out[B] - def[B]); out[B] = U{in[P]},P为B的后继块
+		std::set<symbolItem *> lvOut;
+		std::set<symbolItem *> lvIn;
+		std::set<symbolItem *> use;
+		std::set<symbolItem *> def;
+
+		SymbolTable st;	// 存临时变量和临时常量
     protected:
     private:
 };
