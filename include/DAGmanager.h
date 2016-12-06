b53745a0501aac63ff5278296e269d607596c62d
杨森 <yangsen758@foxmail.com>
2016-12-06 23:25:16 +0800

    improve declaration of class DAGmanager

--- include/DAGmanager.h
+++ include/DAGmanager.h
@@ -1,12 +1,28 @@
 #ifndef DAGMANAGER_H
 #define DAGMANAGER_H
 
+#include "QuaterInstr.h"
+#include <string>
+#include <vector>
 
+struct node
+{
+	int leftchild;
+	int rightchild;
+	std::string name;
+};
 class DAGmanager
 {
     public:
-        DAGmanager();
+		DAGmanager(std::vector<QuaterInstr *> &_block, SymbolTable &_st) : block(_block), st(_st) {};
         virtual ~DAGmanager();
+
+		void buildDAG();
+		void regenerateMiddleCode();
+
+		std::vector<node> nodelist;
+		std::vector<QuaterInstr *> &block;
+		SymbolTable &st;
     protected:
     private:
 };
