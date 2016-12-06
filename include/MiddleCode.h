2ec363b7b592ae0262d6250747f5702b2e040858
杨森 <yangsen758@foxmail.com>
2016-12-06 23:24:49 +0800

    improve optimization of middle code.

--- include/MiddleCode.h
+++ include/MiddleCode.h
@@ -5,22 +5,30 @@
 #include <string>
 #include "QuaterInstr.h"
 #include "SymbolTable.h"
+#include "Block.h"
 
 class MiddleCode
 {
     public:
         MiddleCode();
         virtual ~MiddleCode();
-        QuaterInstr* gen(Opcode _op, struct symbolItem* _des = NULL, struct symbolItem* _src1 = NULL, struct symbolItem* _src2 = NULL);
+		bool isJumpOrBranch(Opcode _op);		// 判断四元式的操作数是否为 jump or branch, 具体为 BGT BGE BLT BLE BEQ BNE BNZ BEZ JUMP
+		void updateFlag(QuaterInstr * new_quater_instr);	// 根据生成的指令更新flag数组
+		QuaterInstr* gen(Opcode _op, struct symbolItem* _des = NULL, struct symbolItem* _src1 = NULL, struct symbolItem* _src2 = NULL);
 		QuaterInstr* gen(Opcode _op, std::string label, struct symbolItem* _src1 = NULL, struct symbolItem* _src2 = NULL);
 
-		void clear();	// 清空中间代码缓冲区
+		void clear();	// 清空中间代码缓冲区 和 基本块数组 和 flag数组
 		void printMiddleCode();	// [调试] 输出这段中间代码
 		void optimize(); // 优化中间代码
+		void load(); // 将中间代码分为基本块, 并加载到基本块数组中
+		void store(); // 将基本块数组中的值, 装回中间代码缓冲区
         
-        std::vector<QuaterInstr*> middle_codes;	// 中间代码缓冲
+		std::vector<int> flag;	// 判断该指令是否是基本块的入口
+		std::vector<Block *> basicBlocks;	// 基本块数组
+        std::vector<QuaterInstr *> middle_codes;	// 中间代码缓冲
     protected:
     private:
+		bool preQuaterInstructionIsJumpOrBranch = false;
 };
 
 #endif // MIDDLECODE_H
--- src/MiddleCode.cpp
+++ src/MiddleCode.cpp
@@ -1,4 +1,5 @@
 #include "MiddleCode.h"
+#include "DAGmanager.h"
 #include <iostream>
 
 using namespace std;
@@ -13,11 +14,29 @@ MiddleCode::~MiddleCode()
     //dtor
 }
 
+bool MiddleCode::isJumpOrBranch(Opcode _op)
+{
+	return _op == Opcode::BGT || _op == Opcode::BGE || _op == Opcode::BLT || _op == Opcode::BLE || _op == Opcode::BEQ || _op == Opcode::BNE || _op == Opcode::BNZ || _op == Opcode::BEZ || _op == Opcode::JUMP;
+}
+
+void MiddleCode::updateFlag(QuaterInstr* new_quater_instr)
+{
+	if (new_quater_instr->op == Opcode::DEC || new_quater_instr->op == Opcode::SET || preQuaterInstructionIsJumpOrBranch == true)
+		flag.push_back(1);
+	else
+		flag.push_back(0);
+	if (isJumpOrBranch(new_quater_instr->op) || new_quater_instr->op == Opcode::RET)
+		preQuaterInstructionIsJumpOrBranch = true;
+	else
+		preQuaterInstructionIsJumpOrBranch = false;
+}
+
 QuaterInstr* MiddleCode::gen(Opcode _op, struct symbolItem* _des, struct symbolItem* _src1, struct symbolItem* _src2) {
     QuaterInstr* new_quater_instr = new QuaterInstr(_op, _des, _src1, _src2);
     //add the quater code to the middle code.
     middle_codes.push_back(new_quater_instr);
-    return new_quater_instr;
+	updateFlag(new_quater_instr);
+	return new_quater_instr;
 }
 
 QuaterInstr * MiddleCode::gen(Opcode _op, std::string label, symbolItem * _src1, symbolItem * _src2)
@@ -25,12 +44,15 @@ QuaterInstr * MiddleCode::gen(Opcode _op, std::string label, symbolItem * _src1,
 	QuaterInstr *new_quater_instr = new QuaterInstr(_op, label, _src1, _src2);
 	//add the quater code to the middle code.
 	middle_codes.push_back(new_quater_instr);
+	updateFlag(new_quater_instr);
 	return new_quater_instr;
 }
 
 void MiddleCode::clear()
 {
 	middle_codes.clear();
+	flag.clear();
+	basicBlocks.clear();
 }
 
 void MiddleCode::printMiddleCode()
@@ -44,4 +66,72 @@ void MiddleCode::printMiddleCode()
     }
 	cout << endl;
 #endif
+}
+
+void MiddleCode::optimize()
+{
+	load();
+
+	for each (auto it in basicBlocks)
+	{
+		DAGmanager dag(it->middleCode, it->st);
+		dag.buildDAG();
+		dag.regenerateMiddleCode();
+	}
+
+	// reaching define
+
+	// live-variable analysis
+
+	store();
+}
+
+void MiddleCode::load()
+{
+	Block *newblock = nullptr, *preblock = nullptr;
+	for (int i = 0; i < flag.size(); i++)
+	{
+		if (flag[i] == 1)
+		{
+			preblock = newblock;
+			newblock = new Block();
+			newblock->middleCode.push_back(middle_codes[i]);
+			if (preblock != nullptr && preblock->middleCode.back()->op != Opcode::JUMP && preblock->middleCode.back()->op != Opcode::RET)	// 更新前驱和后继
+			{
+				newblock->before.push_back(preblock);
+				preblock->after.push_back(newblock);
+			}
+			basicBlocks.push_back(newblock);
+		}
+		else
+		{
+			newblock->middleCode.push_back(middle_codes[i]);
+		}
+	}
+	for (auto it = basicBlocks.begin(); it != basicBlocks.end(); it++)
+	{	// 筛选出最后一条指令为跳转或分支指令的基本块
+		if (isJumpOrBranch((*it)->middleCode.back()->op))
+		{
+			for (auto it2 = basicBlocks.begin(); it != basicBlocks.end(); it2++)
+			{	// 筛选出 第一条指令为set指令, 并且与跳转或分支指令的标签相同 的基本块
+				if ((*it2)->middleCode.front()->op == Opcode::SET || (*it2)->middleCode.front()->label == (*it)->middleCode.back()->label)
+				{
+					(*it2)->before.push_back(*it);
+					(*it)->after.push_back(*it2);
+				}
+			}
+		}
+	}
+}
+
+void MiddleCode::store()
+{
+	middle_codes.clear();
+	for (auto it = basicBlocks.cbegin(); it != basicBlocks.cend(); it++)
+	{
+		for (auto it2 = (*it)->middleCode.cbegin(); it2 != (*it)->middleCode.cend(); it2++)
+		{
+			middle_codes.push_back(*it2);	// 两次循环, 逐个添加
+		}
+	}
 }
